58773e817d42e21d505edcdeb537537ea82bf9ae
seeseal <cur12151@naver.com>
2022-04-26 15:28:24 +0900

    react App 생성 & React Router 실습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+
+import RouteTest from "./components/RouteTest";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
