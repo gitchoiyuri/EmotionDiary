42dd85b3b52b266e3b33bc6865d283def8781311
seeseal <cur12151@naver.com>
2022-04-29 10:21:54 +0900

    New.js에header와 달력 생성

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+
+const getStringDate = (date) => {
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  return `${year}-${month}-${day}`;
+};
+
 const New = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>New</h1>
       <p>이곳은 일기 작성 페이지 입니다.</p>
+      <MyHeader
+        headText={"새 일기 쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
